1293ba164325eac9bc51c59c2882df6d4a741e26
apjanco <apjanco@gmail.com>
2023-08-24 22:18:24 -0400

    starting to parse filename

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,24 @@ const fg = require('fast-glob');
 const ghcImages = fg.sync(['**/GHC/**', '!**/_site']);
 const mfcImages = fg.sync(['**/MFC/**', '!**/_site']);
 
+const createImageObject = (path) => {
+  // Given a path, split on _, return an object with the path
+  //GHC/GHC_B43/EAP1477_GHC_B43_Doc16_IMG_090.jpg
+  let image = {
+    src: path,
+    name: path.split('_')[1],
+  }
+  console.log(image)
+  return Object.create(image);
+}
 //Create collections so you can access the data in your templates
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("GHC");
+
   //Create collection of gallery images
   eleventyConfig.addCollection('ghc', function(collection) {
-    return ghcImages;
+    return ghcImages.map((x) => createImageObject(x));
   });
 
   //Create collection of sponsor logos
